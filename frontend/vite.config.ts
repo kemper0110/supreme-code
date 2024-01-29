de08ee291aeea97c576758e905ecadb82727ac66
Danil <danil2002.2043@gmail.com>
2024-01-29 22:25:14 +0300

    init frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+    server: {
+      proxy: {
+          "/api": {
+              target: "http://localhost:8000/api",
+              changeOrigin: true,
+              secure: false,
+              configure: (proxy, _options) => {
+                  proxy.on('error', (err, _req, _res) => {
+                      console.log('proxy error', err);
+                  });
+                  proxy.on('proxyReq', (proxyReq, req, _res) => {
+                      // console.log('Sending Request to the Target:', req.method, req.url);
+                  });
+                  proxy.on('proxyRes', (proxyRes, req, _res) => {
+                      // console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+                  });
+              },
+          }
+      }
+    }
+})
