a27db334bdcc5d69ff72c6cdca6fa44b29e46d0b
Danil <danil2002.2043@gmail.com>
2024-02-08 21:44:32 +0300

    + 2 repos

---
+++ web/src/main/java/net/danil/web/repository/SolutionRepository.java
@@ -0,0 +1,9 @@
+package net.danil.web.repository;
+
+import net.danil.web.model.Solution;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SolutionRepository extends JpaRepository<Solution, Long> {
+}
