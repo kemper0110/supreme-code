19f22635a90d52c8362db0b36026311f950d179b
Danil <danil2002.2043@gmail.com>
2024-02-20 00:43:53 +0300

    use channel at controller

--- web/src/main/java/net/danil/web/controller/CodePageController.java
+++ web/src/main/java/net/danil/web/controller/CodePageController.java
@@ -3,55 +3,46 @@ package net.danil.web.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.kafka.annotation.KafkaListener;
+import net.danil.web.service.TaskRunnerChannelService;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.MessageHandler;
-import org.springframework.messaging.SubscribableChannel;
-import org.springframework.messaging.support.GenericMessage;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.integration.dsl.MessageChannels;
-
 import reactor.core.publisher.Mono;
+
+import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
 public class CodePageController {
     final private KafkaTemplate<String, String> kafka;
-    private SubscribableChannel resultsChannel;
-    {
-        this.resultsChannel = MessageChannels
-                .publishSubscribe("ResultsChannel")
-                .getObject();
-    }
+    final private TaskRunnerChannelService taskRunnerChannelService;
+
     @GetMapping("/")
     String index() {
         return "index";
     }
 
-    record TaskRequest(String code, String language) {}
+    record TaskRequest(String code, String language) {
+    }
+
     @ResponseBody
     @PostMapping("/api/")
     Mono<String> run(@RequestBody TaskRequest taskRequest) {
-        final var mapper = new ObjectMapper();
         return Mono.create(sink -> {
+            final var taskId = UUID.randomUUID().toString();
             try {
-                kafka.send("task-topic", mapper.writeValueAsString(taskRequest));
+                final var mapper = new ObjectMapper();
+                kafka.send("task-topic", taskId, mapper.writeValueAsString(taskRequest));
             } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
+                sink.error(new RuntimeException(e));
+                return;
             }
-            MessageHandler handler = message -> sink.success((String) message.getPayload());
-            sink.onDispose(() -> resultsChannel.unsubscribe(handler));
-            resultsChannel.subscribe(handler);
+            sink.onDispose(() -> taskRunnerChannelService.unsubscribe(taskId));
+            taskRunnerChannelService.subscribe(taskId, message -> sink.success((String) message.getPayload()));
         });
-    }
-
-    @KafkaListener(topics = "result-topic")
-    public void listen(String in) {
-        System.out.println("received result: " + in);
-        resultsChannel.send(new GenericMessage<>(in));
     }
 }
