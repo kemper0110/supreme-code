5f711e4e44402f57597df58a0b994d5bd5593794
kemper0110 <danil2003.2043@gmail.com>
2024-02-25 16:27:34 +0300

    test for content

--- content-repository/src/test/java/org/danil/ContentRepositoryTest.java
+++ content-repository/src/test/java/org/danil/ContentRepositoryTest.java
@@ -1,12 +1,11 @@
 package org.danil;
 
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class ContentRepositoryTest {
