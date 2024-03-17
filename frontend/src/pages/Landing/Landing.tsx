6d31f48b851a34757eff8483695e207103372b62
Danil <danil2002.2043@gmail.com>
2024-03-17 14:47:36 +0300

    update text colors

--- frontend/src/pages/Landing/Landing.tsx
+++ frontend/src/pages/Landing/Landing.tsx
@@ -17,7 +17,7 @@ function Landing() {
                 className={'from-blue-600 via-green-500 to-indigo-400 font-medium bg-gradient-to-r bg-clip-text text-transparent'}>Достигайте мастерства</span>
                 <span className={'font-medium text-slate-700'}> через вызов</span>
               </h1>
-              <p className={'mt-4 text-[24px] text-slate-800'}>
+              <p className={'mt-4 text-[24px] text-slate-700 font-medium'}>
                 Совершенствуйте свои навыки программирования
               </p>
               <Button mt={30} size={'xl'} onClick={() => navigate('/auth')}>Начать работу</Button>
@@ -30,10 +30,10 @@ function Landing() {
           <Stack mih={'80dvh'}>
             <div className={'bg-slate-300 w-full h-[400px] rounded-2xl flex'}>
               <article className={'p-8 w-1/2'}>
-                <h1 className={'font-bold text-3xl'}>
+                <h1 className={'font-bold text-3xl text-slate-700'}>
                   Оттачивайте свои навыки программирования
                 </h1>
-                <p className={'mt-6 text-lg font-medium'}>
+                <p className={'mt-6 text-lg font-medium text-slate-700'}>
                   Испытайте себя в небольших упражнениях по программированию. Каждое задание разработано создателем, чтобы помочь вам усовершенствовать различные методы программирования. Освоите выбранный вами текущий язык или быстро освоите любой из 3 поддерживаемых языков программирования.
                 </p>
               </article>
