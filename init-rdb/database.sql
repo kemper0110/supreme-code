6a1b4359be2dcf63a0ba73a0ed503063bbacc8dd
Danil <danil2002.2043@gmail.com>
2024-03-19 17:47:03 +0300

    add keycloak

--- init-rdb/database.sql
+++ init-rdb/database.sql
@@ -1,3 +1,7 @@
 create database supreme_code;
 create user supreme_user with password 'password';
 grant all privileges on database supreme_code to supreme_user;
+
+create database keycloak_db;
+create user keycloak_user with password 'password';
+grant all privileges on database keycloak_db to keycloak_user;
