e4a89ce84d9ebd84e5eaadc8610d4d6308fbf37e
Danil <danil2002.2043@gmail.com>
2024-03-23 13:39:49 +0300

    + vitest

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
@@ -23,5 +24,10 @@ export default defineConfig({
               },
           }
       }
-    }
+    },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    mockReset: true,
+  },
 })
