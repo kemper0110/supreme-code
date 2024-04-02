b5052f0d6f1099426463d0b3210d02c4461a1bc9
Danil <danil2002.2043@gmail.com>
2024-04-02 23:23:42 +0300

    add gateway to task-runner

--- web/src/main/java/net/danil/web/controller/PlaygroundController.java
+++ web/src/main/java/net/danil/web/controller/PlaygroundController.java
@@ -1,47 +1,33 @@
 package net.danil.web.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import net.danil.web.service.TaskRunnerChannelService;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.danil.model.Language;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import reactor.core.publisher.Mono;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.UUID;
 
-//@Controller
-//@RequiredArgsConstructor
-//public class PlaygroundController {
-//    final private KafkaTemplate<String, String> kafka;
-//    final private TaskRunnerChannelService taskRunnerChannelService;
-//
-//    @GetMapping("/")
-//    String index() {
-//        return "index";
-//    }
-//
-//    record TaskRequest(String code, String language) {
-//    }
-//
-//    @ResponseBody
-//    @PostMapping("/api/")
-//    Mono<String> run(@RequestBody TaskRequest taskRequest) {
-//        return Mono.create(sink -> {
-//            final var taskId = UUID.randomUUID().toString();
-//            try {
-//                final var mapper = new ObjectMapper();
-//                kafka.send("task-topic", taskId, mapper.writeValueAsString(taskRequest));
-//            } catch (JsonProcessingException e) {
-//                sink.error(new RuntimeException(e));
-//                return;
-//            }
-//            sink.onDispose(() -> taskRunnerChannelService.unsubscribe(taskId));
-//            taskRunnerChannelService.subscribe(taskId, message -> sink.success((String) message.getPayload()));
-//        });
-//    }
-//}
+@Controller
+@RequestMapping("/api/playground")
+@RequiredArgsConstructor
+public class PlaygroundController {
+    @GetMapping("/")
+    String index() {
+        return "index";
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("task-runner", r -> r
+                        .path("/api/playground")
+                        .and()
+                        .predicate(p -> p.getRequest().getMethod().matches("POST"))
+                        .uri("http://localhost:8090/api/task")
+                )
+                .build();
+    }
+}
