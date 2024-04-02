daf8b3dcea83e94748c7dda5cca2e029abd79f9f
Danil <danil2002.2043@gmail.com>
2024-04-02 23:24:08 +0300

    unused deps

--- frontend/src/pages/Playground/Playground.tsx
+++ frontend/src/pages/Playground/Playground.tsx
@@ -1,17 +1,14 @@
 import {Editor} from "@monaco-editor/react";
 import React, {useRef, useState} from "react";
 import {Badge, Button, Flex, Group, SegmentedControl, Text} from "@mantine/core";
-import {useMutation} from "@tanstack/react-query";
 import {editor} from "monaco-editor";
 import {LanguageValue} from "../../types/LanguageValue.tsx";
-import {api} from "../../api/api.ts";
 import {codeExamples} from "./CodeExamples.tsx";
 import {IconArrowAutofitLeft, IconBrain, IconCoin, IconGripVertical, IconMoodCrazyHappy} from "@tabler/icons-react";
 import {Panel, PanelGroup, PanelResizeHandle} from "react-resizable-panels";
 import {Link} from "react-router-dom";
-import ICodeEditor = editor.ICodeEditor;
 import {SSE} from "sse.js";
-import sortBy from "sort-by";
+import ICodeEditor = editor.ICodeEditor;
 
 type RunRequest = {
   code: string
