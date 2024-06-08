ae7a12cdb46baebc6b2be0feb0c635a5986859b8
Danil <danil2002.2043@gmail.com>
2024-06-08 18:35:33 +0300

    http кеш на 1 минутку

--- web/src/main/java/net/danil/web/statistics/StatisticsController.java
+++ web/src/main/java/net/danil/web/statistics/StatisticsController.java
@@ -1,10 +1,14 @@
 package net.danil.web.statistics;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,10 +26,12 @@ public class StatisticsController {
 
 
     @GetMapping
-    Statistics get(@AuthenticationPrincipal(expression = "id") Long userId) {
-        return new Statistics(
-                statisticsService.getGeneralStatistics(),
-                statisticsService.getPersonalStatistics(userId)
-        );
+    ResponseEntity<Statistics> get(@AuthenticationPrincipal(expression = "id") Long userId) {
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(1, TimeUnit.MINUTES))
+                .body(new Statistics(
+                        statisticsService.getGeneralStatistics(),
+                        statisticsService.getPersonalStatistics(userId)
+                ));
     }
 }
